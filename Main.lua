06aa4a0bfde8fd2c61ba0f51e87e730109432d05
Overture <phongsakon00912za@gmail.com>
2024-10-29 00:09:52 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,5 @@
+--ควยเปิดSourceหาพ่อมึงตายไอหน้าหี
+
 local part = Instance.new("Part")
 part.Name = "EmbedSafeOverflow.lolUCanStopmetrashdogfatniggaMucdicdcandteamDev?/Imgonna.top?/overfow.gg/notebySomeonecute"
 part.CFrame = CFrame.new(2760.362548828125, 263.2343444824219, 2701.247314453125)
@@ -29,7 +31,7 @@ elseif currentPlaceId == 6472459099 or currentPlaceId == 6688734180 or currentPl
 elseif currentPlaceId == 7265396387 or currentPlaceId == 7251865082 or currentPlaceId == 7265396805 or currentPlaceId == 7251866503 or currentPlaceId == 7265397072 or currentPlaceId == 7251867155 or currentPlaceId == 7265397848 or currentPlaceId == 7251867574 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Imgonna-Top/Overture-Ultimate/refs/heads/main/Book1chapter4.lua'))() --b1ch4
 elseif currentPlaceId == 6243699076 or currentPlaceId == 15989427413 or currentPlaceId == 7068737459 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Lobby.lua'))() --lobby
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/kuy/TheMimicNew/refs/heads/main/Normal/Lobby.lua'))() --lobby
 elseif currentPlaceId == 7068738088 or currentPlaceId == 7068951438 or currentPlaceId == 7068951914 or currentPlaceId == 7068740106 or currentPlaceId == 7068952294 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Imgonna-Top/Overture-Ultimate/refs/heads/main/Twt.lua'))() --twt
 elseif currentPlaceId == 15996404472 or currentPlaceId == 15996407335 or currentPlaceId == 15996411979 or currentPlaceId == 15996410294 or currentPlaceId == 15996413469 or currentPlaceId == 15996414738 or currentPlaceId == 15996416081 or currentPlaceId == 15996417416 then
@@ -47,11 +49,11 @@ InsertPart("Office", Vector3.new(-1778.0726318359375, 9.717201232910156, -4295.6
 elseif currentPlaceId == 13489800654 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Imgonna-Top/Overture-Ultimate/refs/heads/main/Book2chapter2.lua'))() --b2ch2
 elseif currentPlaceId == 15962819441 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/nil))() --b2ch3
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/User/TheMimicNew/refs/heads/main/Normal/nil'))() --b2ch3
 elseif currentPlaceId == 8311302084 or currentPlaceId == 8311299084 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Imgonna-Top/Overture-Ultimate/refs/heads/main/xmas.lua'))() --Xmas
 elseif currentPlaceId == 11126398230 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Nightmarecircus.lua'))() --nightmarecircus
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Imgonna-Top/Overture-Ultimate/refs/heads/main/circus.lua'))() --nightmarecircus
 elseif currentPlaceId == 7618863566 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Imgonna-Top/Overture-Ultimate/refs/heads/main/Jigoku.lua'))() --Jigoku
 elseif currentPlaceId == 7633631103 or currentPlaceId == 7633631351 or currentPlaceId == 7633631511 then
